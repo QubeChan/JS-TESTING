d623c505b2fd819f533e6ecf956e3ed01b2c9880
QubeChan <jchan122490@gmail.com>
2018-10-29 21:24:56 -0400

    updated testing with different methods of arrays

--- scripts/loggingLetters.js
+++ scripts/loggingLetters.js
@@ -77,8 +77,53 @@
 // }
 // rollDice();
 
-//CODE-ALONG: Defining Functions
-function addTwoNumbers(firstNum, secondNum) {
-  return console.log(firstNum + secondNum);
-}
-addTwoNumbers(1,2);
+// //CODE-ALONG: Defining Functions
+// function addTwoNumbers(firstNum, secondNum) {
+//   return console.log(firstNum + secondNum);
+// }
+// addTwoNumbers(1,2);
+
+// let qube = [10, 20, 30];
+// qube.push(10);
+// console.log(qube);
+// qube.pop();
+// console.log(qube);
+// qube.unshift(10);
+// console.log(qube);
+// qube.shift();
+// console.log(qube);
+
+// let team1=[];
+// let team2=[];
+// let friends = ["Aneki", "Quell", "Clarity", "Sleepy", "Roghar", "DM Crimson"];
+// for (var i = 0; i < friends.length; i++) {
+//   if (i % 2 == 0) {
+//     // if the index is even, add the person to team 1
+//     team1.unshift(friends[i]);
+//     console.log(`team1: ${team1}`);
+//   } else {
+//     // otherwise add them to team 2
+//     team2.unshift(friends[i]);
+//     console.log(`team2: ${team2}`);
+//   }
+//   console.log(friends[i]);
+// }
+
+let fruits = ["kiwi", "rambutan", "mango", "tomato"];
+console.log(fruits);
+console.log(`${fruits.pop()} is taken out from the back of the array using .pop()`);
+console.log(`${fruits} remains in the array`);
+console.log(`"gooseberry" is the ${fruits.push("gooseberry")} item added at the end of the array using .push()`);
+console.log(`${fruits} remains in the array`);
+console.log(`${fruits.shift()} is taken out from the front of the array using .shift()`);
+console.log(`${fruits} remains in the array`);
+console.log(`"banana" is the ${fruits.unshift("banana")} item added to the front of the array using .unshift()`);
+console.log(`${fruits} remains in the array`);
+console.log(`${fruits.slice(0,2)} is what is displayed when using fruits.slice(0,2) to get 2 items out starting from the 1st index starting at position 0`);
+console.log(fruits);
+console.log(`${fruits.splice(0,2)} is what is taken out from the array. fruits.splice(0,2) takes 2 items out starting from the 1st index starting at position 0 and leaves ${fruits} left in the array)`);
+console.log(`${fruits} is left in the array`);
+console.log(`${fruits[fruits.length] = "pineapple"} is added to the end of the fruits[]`);
+console.log(`${fruits} is what the array now holds`);
+console.log(`${fruits[5] = "dragonfruit"} is now added to the 5th index of fruits[]`);
+console.log(`${fruits} is what the array now holds. the 3rd and 4th index now holds undefined`);
