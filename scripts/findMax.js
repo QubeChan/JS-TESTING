15ec9b06da07f795c4f986fc6d91c61218e53e32
QubeChan <jchan122490@gmail.com>
2018-10-30 09:37:14 -0400

    found the max

--- scripts/findMax.js
+++ scripts/findMax.js
@@ -1,15 +1,12 @@
 //Create a function named findMax which accepts one parameter: an array of numbers.
-let arr = [110, 10, 20, 1, 9, 100, 35, 9, 105]
+let arr = [110, 10, 20, 1, 9, 0, 35, 9, 102]
 
 function findMax(arr) {
-  let biggestNum = 0;
+  let biggestNum = arr[0];
   for (var i = 1; i < arr.length; i++) {
-    if (arr[i - 1] < arr[i]) {
+    if (arr[i] > arr[i - 1]  && arr[i] > biggestNum) {
       biggestNum = arr[i];
     }
-  }
-  if (arr[0] > biggestNum) {
-    biggestNum = arr[0];
   }
   console.log(biggestNum);
 }
